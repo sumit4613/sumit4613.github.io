39f8eabfd554f598fc2e70982df3ca4b66cd9b4c
Sumit Singh <sumiterw11@gmail.com>
2019-03-30 16:25:04 +0530

    added new effects

--- script.js
+++ script.js
@@ -16,31 +16,44 @@
 
  	$('.owl-carousel').owlCarousel({
 	    loop:true,
-	    margin:10,
-	    nav:true,
+	    items: 4,
 	    responsive:{
 	        0:{
 	            items:1
 	        },
-	        600:{
+	        480:{
+	            items:2
+	        },
+	        768:{
 	            items:3
 	        },
-	        1000:{
+	        938:{
+	            items:4
+	        },
+	        1280:{
 	            items:5
 	        }
 	    }
 	});
 
-	$('.chart').easyPieChart({
-		easing: 'easeInOut',
-		barColor: '#fff',
-		trackColor: false,
-		scaleColor: false,
-		lineWidth: 4,
-		size: 152,
-		onStep: function(from, to, percent) {
-			$(this.el).find('.percent').text(Math.round(percent));
-		}
+    var skillsTopOffset = $(".skillsSection").offset().top;
+
+    $(window).scroll(function() {
+    	if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+    		$('.chart').easyPieChart({
+				easing: 'easeInOut',
+				barColor: '#fff',
+				trackColor: false,
+				scaleColor: false,
+				lineWidth: 4,
+				size: 152,
+				onStep: function(from, to, percent) {
+					$(this.el).find('.percent').text(Math.round(percent));
+				}
+		    });
+    	}
     });
+
+    
 
  });
