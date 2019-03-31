5062f4d4371738eeed6b75c223a39ff42c408d9c
Sumit Singh <sumiterw11@gmail.com>
2019-03-31 13:53:28 +0530

    added portfolio section

--- script.js
+++ script.js
@@ -68,6 +68,33 @@
 
     });
 
+    $("[data-fancybox]").fancybox();
+
+    $(".items").isotope({
+    	filter: '*',
+    	animationOptions: {
+    		duration: 1500,
+    		easing: 'linear',
+    		queue: false
+    	}
+    });
+
+    $("#filters a").click(function() {
+    	$("#filters .current").removeClass("current");
+    	$(this).addClass("current");
+
+    	var selector = $(this).attr("data-filter");
+
+    	$(".items").isotope({
+	    	filter: selector,
+	    	animationOptions: {
+	    		duration: 1500,
+	    		easing: 'linear',
+	    		queue: false
+	    	}
+    	});
+    	return false;
+    });
     
 
  });
