bd46fab7b775ff5c433ae085773526fe474fe871
Sumit Singh <sumiterw11@gmail.com>
2019-03-31 16:06:21 +0530

    added navigation bar

--- script.js
+++ script.js
@@ -95,6 +95,35 @@
     	});
     	return false;
     });
+
+
+    $("#navigation li a").click(function(e) {
+    	e.preventDefault();
+
+    	var targetElement = $(this).attr("href");
+    	var targetPosition = $(targetElement).offset().top;
+    	$("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+    });
+
+
+
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+
+    	var body = $("body");
+    	if($(window).scrollTop() >= navTop) {
+    		body.css("padding-top", nav.outerHeight() + "px");
+    		body.addClass("fixedNav");    		
+    	}
+    	else {
+    		body.css("padding-top", 0);
+    		body.removeClass("fixedNav");
+    	}
+    }
     
 
  });
