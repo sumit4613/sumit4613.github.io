78c068b7806b81cc5a1c2519d09950f61d803273
Sumit Singh <sumiterw11@gmail.com>
2019-04-20 02:00:48 +0530

    major changes

--- script.js
+++ script.js
@@ -1,136 +1,120 @@
 $(window).on("load", function() {
-	$(".loader .inner").fadeOut(500, function() {
-		$(".loader").fadeOut(750);
-	});
+    $(".loader .inner").fadeOut(500, function() {
+        $(".loader").fadeOut(750);
+    });
 
-	$(".items").isotope({
-    	filter: '*',
-    	animationOptions: {
-    		duration: 1500,
-    		easing: 'linear',
-    		queue: false
-    	}
+    // Portfolio
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
     });
 })
 
 
 $(document).ready(function() {
+    // Scroll
+    $('.scrollto').click(function(e) {
+        e.preventDefault();
+        var scrollElm = $(this).attr('href');
+        var scrollTo = $(scrollElm).offset().top;
+        $('html, body').animate({ scrollTop: scrollTo - 50}, "slow");
+    })
+    // Slides
+    $('#slides').superslides({
+        animation: 'fade',
+        play: 5000,
+        pagination: false
+    });
 
- 	$('#slides').superslides({
- 		animation: 'fade',
- 		play: 4000,
- 		pagination: false
- 	});
+    var typed = new Typed(".typed", {
+        strings: ["Student.", "Web Developer.", "Python Developer.", "Blockchain Enthusiast."],
+        typeSpeed: 90,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
 
- 	var typed = new Typed(".typed", {
- 		strings: ["Student.", "Web Developer.", "Python Developer.", "Blockchain Enthusiast."],
- 		typeSpeed: 90,
- 		loop: true,
- 		startDelay: 1000,
- 		showCursor: false
- 	});
+    // Skills
+    var owl = $("#skills-slide")
+    owl.owlCarousel({
+        autoPlay:true,
+        items: 4,
+        itemsDesktop:[1000,4],
+        itemsDesktopSmall:[900,3],
+        itemsTablet:[600,2],
+        itemsMobile:[480,1],
+    });
 
- 	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    items: 4,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        480:{
-	            items:2
-	        },
-	        768:{
-	            items:3
-	        },
-	        938:{
-	            items:4
-	        },
-	        1280:{
-	            items:5
-	        }
-	    }
-	});
+    // Animatons
+    var windowH = $(window).height();
 
-    var skillsTopOffset = $(".skillsSection").offset().top;
-    var statsTopOffset = $(".statsSection").offset().top;
-    var countUpFinished = false;
+    $(window).bind('resize', function () {
+        windowH = $(window).height();
+    });
+    $('.hidethis').bind('inview', function (event, visible) {
+        if (visible === true) {
+            $(this).removeClass('hidethis');
+        }
+    });
+    var servicesTopOffset = $(".about .about-inner").offset().top;
+    var timelineTopOffset = $(".timeline").offset().top;
+    var skillsTopOffset = $('.skills').offset().top;
 
     $(window).scroll(function() {
-    	if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-    		$('.chart').easyPieChart({
-				easing: 'easeInOut',
-				barColor: '#fff',
-				trackColor: false,
-				scaleColor: false,
-				lineWidth: 4,
-				size: 152,
-				onStep: function(from, to, percent) {
-					$(this.el).find('.percent').text(Math.round(percent));
-				}
-		    });
-    	}
+        // fixed navbar
+        if(window.pageYOffset > windowH)
+        {
+            $('.navbar-flat').addClass('navbar-fixed-top');
+            $('.firstSec').addClass('fixed');
+        }
+        else
+        {
+            $('.navbar-flat').removeClass('navbar-fixed-top');
+            $('.firstSec').removeClass('fixed');
+        }
+        // Timeline animation
+        if(window.pageYOffset > timelineTopOffset-windowH+200)
+        {
+            $('.timeline li').addClass('fadeInUp');
+        }
 
-    	if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
-    		$(".counter").each(function () {
-		    	var element = $(this);
-		    	var duration = 10;
-		    	var endVal = parseInt(element.text());
-		    	element.countup(endVal);
-		    })
-
-		    countUpFinished = true;
-    	}
-
+        // Skills Chart Animation
+        if(window.pageYOffset > skillsTopOffset - windowH + 200) {
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+        }
     });
 
     $("[data-fancybox]").fancybox();
 
     $("#filters a").click(function() {
-    	$("#filters .current").removeClass("current");
-    	$(this).addClass("current");
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
 
-    	var selector = $(this).attr("data-filter");
+        var selector = $(this).attr("data-filter");
 
-    	$(".items").isotope({
-	    	filter: selector,
-	    	animationOptions: {
-	    		duration: 1500,
-	    		easing: 'linear',
-	    		queue: false
-	    	}
-    	});
-    	return false;
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        return false;
     });
 
-
-    $("#navigation li a").click(function(e) {
-    	e.preventDefault();
-
-    	var targetElement = $(this).attr("href");
-    	var targetPosition = $(targetElement).offset().top;
-    	$("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
-    });
-
-
-
-    const nav = $("#navigation");
-    const navTop = nav.offset().top;
-
-    $(window).on("scroll", stickyNavigation);
-
-    function stickyNavigation() {
-
-    	var body = $("body");
-    	if($(window).scrollTop() >= navTop) {
-    		body.css("padding-top", nav.outerHeight() + "px");
-    		body.addClass("fixedNav");    		
-    	}
-    	else {
-    		body.css("padding-top", 0);
-    		body.removeClass("fixedNav");
-    	}
-    }
-    
-
- });
+});
